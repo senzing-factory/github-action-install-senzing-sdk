d629382edd0a78b04a9ac4abd05055c834cb28ff
Sam <109683132+kernelsam@users.noreply.github.com>
2025-01-13 13:47:13 -0800

    cleanup linting

--- darwin/install-senzing.sh
+++ darwin/install-senzing.sh
@@ -84,7 +84,7 @@ is-major-version-greater-than-3() {
 ############################################
 determine-latest-dmg-for-major-version() {
 
-  aws s3 ls $SENZINGSDK_URI --recursive --no-sign-request | grep -o -E '[^ ]+.dmg$' > /tmp/staging-versions
+  aws s3 ls "$SENZINGSDK_URI" --recursive --no-sign-request | grep -o -E '[^ ]+.dmg$' > /tmp/staging-versions
   latest_staging_version=$(< /tmp/staging-versions grep "_$MAJOR_VERSION" | sort -r | head -n 1)
   rm /tmp/staging-versions
   echo "[INFO] latest staging version is: $latest_staging_version"
